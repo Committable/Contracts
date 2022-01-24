4bb98004b9145e558d2dde04f10bd65d4ec83b73
aolin <523694744@qq.com>
2022-01-24 18:06:29 +0800

    add airdropPool contract & test

---
+++ contracts/reward/AirdropPool.sol
@@ -0,0 +1,154 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "../Controller.sol";
+import "../library/ECDSA.sol";
+
+contract AirdropPool is Ownable, ReentrancyGuard {
+    Controller internal _controller;
+    PoolInfo[] public poolInfo;
+    mapping(uint256 => mapping(address => UserInfo)) public userInfo;
+    struct UserInfo {
+        bool isClaimed;
+    }
+    struct PoolInfo {
+        address creator;
+        IERC20 rewardToken;
+        uint256 rewardAmount;
+        uint256 unclaimedAmount;
+        uint256 start;
+        uint256 end;
+    }
+
+    event PoolCreated(
+        uint256 indexed index,
+        address indexed rewardToken,
+        uint256 rewardAmount,
+        address indexed creator,
+        uint256 start,
+        uint256 end
+    );
+
+    event RewardClaimed(
+        uint256 indexed index,
+        address indexed rewardToken,
+        uint256 rewardAmount,
+        address indexed user
+    );
+
+    constructor(address controller) {
+        _controller = Controller(controller);
+    }
+
+    /**
+     * @dev return pool info by index
+     */
+    function getPoolInfo(uint256 index)
+        external
+        view
+        returns (PoolInfo memory)
+    {
+        return poolInfo[index];
+    }
+
+    /**
+     * @dev return user info by pool index and address, return true when user has claimed the token  
+     */
+    function getUserInfo(uint256 index, address user)
+        external
+        view
+        returns (bool)
+    {
+        return userInfo[index][user].isClaimed;
+    }
+
+    /**
+     * @dev create airdrop pool and provide reward tokens
+     */
+    function create(
+        address rewardToken,
+        uint256 rewardAmount,
+        uint256 start,
+        uint256 end
+    ) external nonReentrant {
+        SafeERC20.safeTransferFrom(
+            IERC20(rewardToken),
+            msg.sender,
+            address(this),
+            rewardAmount
+        );
+        uint256 index = poolInfo.length;
+        poolInfo.push(
+            PoolInfo({
+                creator: msg.sender,
+                rewardToken: IERC20(rewardToken),
+                rewardAmount: rewardAmount,
+                unclaimedAmount: rewardAmount,
+                start: start,
+                end: end
+            })
+        );
+
+        emit PoolCreated(
+            index,
+            rewardToken,
+            rewardAmount,
+            msg.sender,
+            start,
+            end
+        );
+    }
+
+    /**
+     * @dev claim airdrop tokens, must provide server signature
+     */
+    function claim(
+        uint256 index,
+        uint256 amount,
+        bytes memory sig
+    ) external nonReentrant {
+        require(
+            index < poolInfo.length,
+            "index out of bounds"
+        );
+        require(
+            block.timestamp > poolInfo[index].start &&
+                block.timestamp < poolInfo[index].end,
+            "invalid timestamp"
+        );
+        require(!userInfo[index][msg.sender].isClaimed, "claim once only");
+        bytes32 hash = keccak256(abi.encode(index, amount, msg.sender));
+        require(
+            ECDSA.recover(hash, sig) == _controller.getSigner(),
+            "invalid signature"
+        );
+        userInfo[index][msg.sender].isClaimed = true;
+        poolInfo[index].unclaimedAmount -= amount;
+        IERC20 rewardToken = poolInfo[index].rewardToken;
+        SafeERC20.safeTransfer(rewardToken, msg.sender, amount);
+        emit RewardClaimed(index, address(rewardToken), amount, msg.sender);
+    }
+
+    /**
+     * @dev creator can withdrawl unclaimed rewardToken after end-time
+     */
+    function withdraw(uint256 index) external nonReentrant {
+        require(msg.sender == poolInfo[index].creator, "only creator");
+        require(block.timestamp >= poolInfo[index].end, "invalid timestamp");
+        IERC20 rewardToken = poolInfo[index].rewardToken;
+        uint256 unclaimedAmount = poolInfo[index].unclaimedAmount;
+        SafeERC20.safeTransfer(rewardToken, msg.sender, unclaimedAmount);
+        emit RewardClaimed(
+            index,
+            address(rewardToken),
+            unclaimedAmount,
+            msg.sender
+        );
+        poolInfo[index].unclaimedAmount = 0;
+    }
+}
---
+++ test/Airdrop.test.js
@@ -0,0 +1,130 @@
+const { expect } = require("chai");
+const { ethers, network } = require("hardhat");
+const { constants } = require('@openzeppelin/test-helpers');
+const { NAME, SYMBOL } = require('../.config.js');
+const { ZERO_ADDRESS } = constants;
+
+describe('AirdropPool', function () {
+  context('with deployed contracts', function () {
+    beforeEach(async function () {
+
+      /* get signers */
+      [creator, signer, user, ...others] = await ethers.getSigners();
+      /* deploy helper */
+      const Helper = await ethers.getContractFactory('Helper');
+      helper = await Helper.deploy();
+      await helper.deployed();
+      /* deploy controller contract */
+      let Controller = await ethers.getContractFactory("Controller");
+      controller = await Controller.deploy();
+      await controller.deployed();
+      tx = await controller.setSigner(signer.address);
+      await tx.wait()
+      /* deploy airdropPool */
+      let AirdropPool = await ethers.getContractFactory("AirdropPool");
+      airdropPool = await AirdropPool.deploy(controller.address);
+      await airdropPool.deployed();
+      /* deploy erc20 and approve for test */
+      let ERC20 = await ethers.getContractFactory("ERC20Test");
+      token = await ERC20.connect(creator).deploy("Tether", "USDT");
+      await token.deployed();
+      tx = await token.approve(airdropPool.address, ethers.utils.parseEther('10000').toString());
+      await tx.wait();
+      /* get block info */
+      blockInfo = await ethers.provider.getBlock("latest");
+      /* create an airdrop */
+      start = '0';
+      end = '10000000000'
+      // current = '1643013611'
+      rewardAmount = '10000'
+      tx = await airdropPool.create(token.address, rewardAmount, start, end);
+      await tx.wait()
+      /* sign a user airdrop */
+      abiCoder = new ethers.utils.AbiCoder();
+      index = '0'
+      claimAmount = '100'
+      hash = ethers.utils.keccak256(abiCoder.encode(['uint256', 'uint256', 'address'], [index, claimAmount, user.address]));
+      sig = await signer.signMessage(ethers.utils.arrayify(hash));
+    })
+
+    it.only('should return poolInfo', async function () {
+      let result = await airdropPool.getPoolInfo(0);
+      expect(result.creator).to.equal(creator.address);
+      expect(result.rewardToken).to.equal(token.address);
+      expect(result.rewardAmount).to.equal(rewardAmount);
+      expect(result.unclaimedAmount).to.equal(rewardAmount);
+      expect(result.start).to.equal(start);
+      expect(result.end).to.equal(end);
+    })
+    it.only('should claim airdrop successfully', async function () {
+      let tx = await airdropPool.connect(user).claim(index, claimAmount, sig);
+      expect(tx).to.emit(airdropPool, 'RewardClaimed').withArgs(index, token.address, claimAmount, user.address);
+      expect(tx).to.emit(token, 'Transfer').withArgs(airdropPool.address, user.address, claimAmount);
+      await tx.wait()
+      let poolInfo = await airdropPool.getPoolInfo(index);
+      expect(poolInfo.unclaimedAmount).to.equal(poolInfo.rewardAmount.sub(claimAmount));
+      let userInfo = await airdropPool.getUserInfo(index, user.address);
+      expect(userInfo).to.equal(true)
+    })
+    it.only('cannot claim twice', async function () {
+      let tx = await airdropPool.connect(user).claim(index, claimAmount, sig);
+      await tx.wait();
+      try {
+        await airdropPool.connect(user).claim(index, claimAmount, sig);
+      } catch (err) {
+        expect(err.message).to.include("claim once only")
+      }
+    })
+    it.only('cannot claim with invalid sig', async function () {
+      let invalidSig = await user.signMessage(ethers.utils.arrayify(hash));
+      try {
+        let tx = await airdropPool.connect(user).claim(index, claimAmount, invalidSig);
+        await tx.wait();
+        throw null;
+      } catch (err) {
+        expect(err.message).to.include("invalid signature")
+      }
+    })
+    it.only('cannot claim amount that succeed unclaimed amount', async function () {
+      /* sign a user airdrop */
+      let index = '0'
+      let claimAmount = '100000'
+      let hash = ethers.utils.keccak256(abiCoder.encode(['uint256', 'uint256', 'address'], [index, claimAmount, user.address]));
+      let sig = await signer.signMessage(ethers.utils.arrayify(hash));
+      try {
+        let tx = await airdropPool.connect(user).claim(index, claimAmount, sig)
+        await tx.wait()
+        throw null;
+      } catch (err) {
+        expect(err.message).to.include('underflowed');
+      }
+    })
+    it.only("should revert invalid request", async function () {
+      /* sign a user airdrop */
+      let index = '1'
+      let claimAmount = '10000'
+      let hash = ethers.utils.keccak256(abiCoder.encode(['uint256', 'uint256', 'address'], [index, claimAmount, user.address]));
+      let sig = await signer.signMessage(ethers.utils.arrayify(hash));
+      try {
+        let tx = await airdropPool.connect(user).claim(index, claimAmount, sig)
+        await tx.wait()
+        throw null;
+      } catch (err) {
+        expect(err.message).to.include('index out of bounds');
+      }
+    })
+    it.only('should claim unclaimed token after end-time', async function () {
+      // set net block timestamp to end-time
+      await network.provider.send("evm_setNextBlockTimestamp", [10000000000])
+      let poolInfo = await airdropPool.getPoolInfo(index);
+      let unclaimedAmount = poolInfo.unclaimedAmount;
+      let tx = await airdropPool.connect(creator).withdraw(index);
+      expect(tx).to.emit(airdropPool, 'RewardClaimed').withArgs(index, token.address, unclaimedAmount, creator.address);
+      expect(tx).to.emit(token, 'Transfer').withArgs(airdropPool.address, creator.address, unclaimedAmount);
+      poolInfo = await airdropPool.getPoolInfo(index);
+      unclaimedAmount = poolInfo.unclaimedAmount;
+      expect(unclaimedAmount).to.equal(0)
+    })
+    
+  })  
+})
