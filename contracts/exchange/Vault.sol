4c48189b0b74360aeaf7786801ab525cc6d97f94
aolin <523694744@qq.com>
2022-07-05 15:14:30 +0800

    implement vault contract

---
+++ contracts/exchange/Vault.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract Vault is OwnableUpgradeable, ReentrancyGuardUpgradeable {
+    function initialize() public initializer {
+        __Ownable_init();
+        __ReentrancyGuard_init();
+    }
+
+    function sendEther(address to, uint256 amount) external onlyOwner {
+        payable(to).transfer(amount);
+    }
+
+    function sendERC20(
+        address token,
+        address to,
+        uint256 amount
+    ) external onlyOwner {
+        SafeERC20.safeTransfer(IERC20(token), to, amount);
+    }
+
+    uint256[50] private __gap;
+}
